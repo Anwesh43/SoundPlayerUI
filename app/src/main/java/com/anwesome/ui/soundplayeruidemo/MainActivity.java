5a13772255b6387cb10ef8282ac09c902bfb9519
Anwesh43 <anweshthecool0@gmail.com>
2017-05-21 01:11:17 +0530

    Using SoundPlayerUI in app

--- app/src/main/java/com/anwesome/ui/soundplayeruidemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/soundplayeruidemo/MainActivity.java
@@ -1,13 +1,22 @@
 package com.anwesome.ui.soundplayeruidemo;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+
+import com.anwesome.ui.soundplayerui.SoundPlayerView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SoundPlayerView soundPlayerView = new SoundPlayerView(this,40);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        soundPlayerView.setX(0);
+        soundPlayerView.setY(1400);
+        addContentView(soundPlayerView,new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,200));
     }
 }
